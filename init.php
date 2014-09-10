ce0bdba392fa4af6870f165cc13b8c2ceb848375
kasl <roman.muratov@gmail.com>
2014-09-10 16:34:15 +0700

    init.php delete

--- init.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-require_once(Kohana::find_file('vendor/oauth2-server-php/src', 'OAuth2/Autoloader'));
-
-OAuth2_Autoloader::register();
